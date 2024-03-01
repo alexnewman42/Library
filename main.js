26ef7048eaead4989aff7317612f9a5a4ea88ce7
root <alexnewman42@gmail.com>
2024-03-01 08:53:15 -0500

    Fix syntax error and test

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.infoString = this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
     this.info = function() {
-        return(this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read);
-    }
+        return this.infoString;
+    };
 }
 
-const theHobbit("The Hobbit", "J.R.R. Tolkein", 295, "not read yet");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", 295, "not read yet");
 
 console.log(theHobbit.info());
