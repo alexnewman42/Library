a5c67d58edc181f7cee4ea9c2c5a2ff2d078a782
root <alexnewman42@gmail.com>
2024-04-22 08:45:58 -0400

    Add event listener to add book button

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 let myLibrary = [];
 
+
 function Book() {
 
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function () {
+    let newBookForm = document.querySelector('#new-book-form');
+    newBookForm.style.display = "block";
+});
