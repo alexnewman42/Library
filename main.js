443d0852b6e8d5dbf89eb94a43268f75630db45d
root <alexnewman42@gmail.com>
2024-05-07 08:51:42 -0400

    Add const variables for elements

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-let myLibrary = [];
+const bookForm = document.getElementById("book-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openBookFormBtn = document.getElementById("open-book-form-btn");
+const closeBookFormBtn = document.getElementById("close-book-form-btn");
+const addOrUpdateBookBtn = document.getElementById("add-or-update-book-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const booksContainer = document.getElementById("books-container");
+const titleInput = document.getElementById("title-input");
+const authorInput = document.getElementById("author-input");
+const pagesInput = document.getElementById("pages-input");
+const readInput = document.getElementById("read-input");
+const descriptionInput = document.getElementById("description-input");
 
-
-function Book() {
-
-}
-
-function addBookToLibrary() {
-
-}
-
-let newBookbtn = document.querySelector("#new-book-btn");
-newBookbtn.addEventListener("click", function () {
-    let newBookForm = document.querySelector('#new-book-form');
-    newBookForm.style.display = "block";
-});
