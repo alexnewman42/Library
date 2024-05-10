744d590b97f9333cccbb6efd10bc2887779ed902
root <alexnewman42@gmail.com>
2024-05-10 08:49:42 -0400

    Split the submit event listener function into 2 smaller functions. AddOrUpdateBook and updateBookContainer

--- main.js
+++ main.js
@@ -14,6 +14,35 @@ const readInput = document.getElementById("read-input");
 const bookData = [];
 let currentBook = {};
 
+const addOrUpdateBook = () => {
+    const dataArrIndex = bookData.findIndex((item) => item.id === currentBook.id);
+    const bookObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        author: authorInput.value,
+        pages: pagesInput.value,
+        read: readInput.value,
+    };
+    if (dataArrIndex === -1) {
+        bookData.unshift(bookObj);
+    }
+};
+
+const updateBookContainer = () => {
+    bookData.forEach(({id, title, author, pages, read}) => {
+        (booksContainer.innerHTML += `
+            <div class="book" id="${id}"></div>
+            <p><strong>Title: </strong>${title}</p>
+            <p><strong>Author: </strong>${author}</p>
+            <p><strong>Pages: </strong>${pages}</p>
+            <p><strong>Read: </strong>${read}</p>
+            <button type="button" class="btn">Edit</button>
+            <button type="button" class="btn">Delete</button>
+        `)
+        }
+    );
+};
+
 const reset = () => {
     titleInput.value = "";
     authorInput.value = "";
@@ -32,35 +61,15 @@ closeBookFormBtn.addEventListener("click", () => {
         reset();
     }
 });
-cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close(
+));
 discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
     reset();
 });
 bookForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const dataArrIndex = bookData.findIndex((item) => item.id === currentBook.id);
-    const bookObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        author: authorInput.value,
-        pages: pagesInput.value,
-        read: readInput.value,
-    };
-    if (dataArrIndex === -1) {
-        bookData.unshift(bookObj);
-    }
-    bookData.forEach(({id, title, author, pages, read}) => {
-        (booksContainer.innerHTML += `
-            <div class="book" id="${id}"></div>
-            <p><strong>Title: </strong>${title}</p>
-            <p><strong>Author: </strong>${author}</p>
-            <p><strong>Pages: </strong>${pages}</p>
-            <p><strong>Read: </strong>${read}</p>
-            <button type="button" class="btn">Edit</button>
-            <button type="button" class="btn">Delete</button>
-        `)
-        }
-    );
+
+
     reset();
 });
